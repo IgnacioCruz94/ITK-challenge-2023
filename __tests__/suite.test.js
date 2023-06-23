3b5b4dff126c61af097c21961fe9ec50274a41ef
José Ignacio Cruz Reyes <ignacio.cruz@theksquaregroup.com>
2023-06-23 01:59:22 -0600

    matrix addition challenge

---
+++ __tests__/suite.test.js
@@ -0,0 +1,85 @@
+const { sumOfMatrices } = require("../exercise");
+
+describe("Code Challenge: Sum of matrices", () => {
+  it("function should be defined", () => {
+    expect(sumOfMatrices).toBeDefined();
+  });
+
+  it("Should return the expected result array 1", () => {
+    const firstMatrix = [
+      [1, 2],
+      [3, 4],
+    ];
+    const secondMatrix = [
+      [5, 6],
+      [7, 8],
+    ];
+    const expectedResult = [
+      [6, 8],
+      [10, 12],
+    ];
+    expect(sumOfMatrices(firstMatrix, secondMatrix)).toEqual(expectedResult);
+  });
+
+  it("Should return the expected result array 2", () => {
+    const firstMatrix = [
+      [-1, -2],
+      [-3, -4],
+    ];
+    const secondMatrix = [
+      [-5, -6],
+      [-7, -8],
+    ];
+    const expectedResult2 = [
+      [-6, -8],
+      [-10, -12],
+    ];
+
+    expect(sumOfMatrices(firstMatrix, secondMatrix)).toEqual(expectedResult2);
+  });
+
+  it("Should return the expected result array 3", () => {
+    const firstMatrix = [
+      [0, 0],
+      [0, 0],
+    ];
+    const secondMatrix = [
+      [1, 1],
+      [1, 1],
+    ];
+    const expectedResult = [
+      [1, 1],
+      [1, 1],
+    ];
+
+    expect(sumOfMatrices(firstMatrix, secondMatrix)).toEqual(expectedResult);
+  });
+
+  it("Should throw an error due to invalid matrices size.", () => {
+    const firstMatrix = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+    const secondMatrix = [
+      [10, 11, 12],
+      [13, 14, 15],
+    ];
+
+    expect(() => {
+      sumOfMatrices(firstMatrix, secondMatrix);
+    }).toThrow("Matrices must have the same number of rows and columns.");
+  });
+
+  it("Should throw an error due to empty matrix.", () => {
+    const firstMatrix = [];
+    const secondMatrix = [
+      [10, 11, 12],
+      [13, 14, 15],
+    ];
+
+    expect(() => {
+      sumOfMatrices(firstMatrix, secondMatrix);
+    }).toThrowError("Empty mtrices are not allowed.");
+  });
+});
---
+++ exercise.js
@@ -0,0 +1,41 @@
+/**
+ * Write a JavaScript function that takes two matrices of integers as input and returns
+ * a new matrix that is the sum of the two input matrices. Both input matrices will have
+ * the same number of rows and columns. The sum of the matrices is calculated by adding
+ * the corresponding elements from both matrices.
+ *
+ * Instructions:
+ * - Add your logic in sumOfMatrices function, do not modify the arguments.
+ * - Verify that both matrices have the same number of rows and columns. Otherwise,
+ *   throw an exception with the message "Matrices must have the same number of rows and columns."
+ * - Verify that none is empty, otherwise, throw an exception with the message "Empty mtrices are not allowed."
+ *
+ * Example:
+ * Given the following input matrices:
+ *
+ * Matrix 1:
+ * [ [1, 2, 3],
+ *   [4, 5, 6],
+ *   [7, 8, 9] ]
+ *
+ * Matrix 2:
+ * [[10, 11, 12],
+ * [13, 14, 15],
+ * [16, 17, 18]]
+ *
+ * The function should return the following matrix:
+ *
+ * Output Matrix:
+ * [[11, 13, 15],
+ * [17, 19, 21],
+ * [23, 25, 27]]
+ *
+ */
+
+/** DO NOT CHANGE THE FUNCTION NAME **/
+const sumOfMatrices = (firstMatrix, secondMatrix) => {
+  /* Add your code here, do your best! */
+};
+
+/** DO NOT CHANGE THE LINE BELOW **/
+module.exports.sumOfMatrices = sumOfMatrices;
